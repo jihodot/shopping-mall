e94fef244cce698d07d9cf362266719cb78062c0
jihod <jihodot@gmail.com>
2020-07-26 22:08:54 +0900

    first commit

---
+++ server/routes/product.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req,res)=>{
+})
+
+module.exports = router;
